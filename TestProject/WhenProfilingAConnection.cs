e2cb77db7a0d51e39dc445bec4cc3c738d47ad0b
Oscar Marin <oscar.marin@entegreat.com>
2013-06-21 20:04:46 -0600

    new test case

--- TestProject/WhenProfilingAConnection.cs
+++ TestProject/WhenProfilingAConnection.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Data.SqlServerCe;
 using System.Diagnostics;
 using System.Linq;
@@ -16,12 +17,29 @@ namespace TestProject
         [Test]
         public void ItShouldProvideInformationAboutAnOpeningAndClosingAConnection()
         {
-            var connection = new SqlCeConnection(@"Data Source=.\Data\SampleData.sdf;Persist Security Info=False;");
-            var traceConnection = new DbTraceConnection(connection);
+            var traceConnection = GetConnection();
             traceConnection.Open();
             traceConnection.Close();
             Assert.IsTrue(traceConnection.Tracer.Events.Any(i => i.OperationType == DbTraceOperationType.OpenConnection));
             Assert.IsTrue(traceConnection.Tracer.Events.Any(i => i.OperationType == DbTraceOperationType.CloseConnection));
+        }
+
+        [Test]
+        public void ItShouldProfileProperlyASelectStatement()
+        {
+            var traceConnection = GetConnection();
+            var command = traceConnection.CreateCommand();
+            command.CommandText = "select * from Items where Id = 5";
+            traceConnection.Open();
+            command.ExecuteScalar();
+            traceConnection.Close();
+            Assert.IsTrue(traceConnection.Tracer.Events.Any(i => i.Command == command.CommandText));
+        }
+
+        private DbTraceConnection GetConnection()
+        {
+            var connection = new SqlCeConnection(@"Data Source=.\Data\SampleData.sdf;Persist Security Info=False;");
+            return new DbTraceConnection(connection);
         }
     }
 }
